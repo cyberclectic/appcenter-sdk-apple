1469d0c800366e49c7266f6221165083d32fa3b1
Ruslan Urmeev <v-ruurme@microsoft.com>
2020-04-17 17:44:44 +0400

    update

--- Scripts/build-ohttpstubs.sh
+++ Scripts/build-ohttpstubs.sh
@@ -9,11 +9,16 @@ echo "Build OHTTPStubs $1"
 maxXcodeVersion=1100
 swiftVersion="5.0"
 buildPlatform=$1
+buildSdk=$2
+buildPlatformScheme=$3
 
 cd ../Vendor/OHHTTPStubs
 
 if [ ${XCODE_VERSION_ACTUAL} -lt ${maxXcodeVersion} ]
 then
+    # workaround for the problem on xcode 10: https://stackoverflow.com/questions/52987843/carthage-fails-to-start-when-running-from-xcode-10-build-pre-action/53136006
+    unset LLVM_TARGET_TRIPLE_SUFFIX
+
     #xcode 10
     swiftVersion="4.2"
     sed -i '' -e 's/SWIFT_VERSION = 5.0/SWIFT_VERSION = 4.2/g' OHHTTPStubs.xcodeproj/project.pbxproj
@@ -22,13 +27,10 @@ else
     sed -i '' -e 's/SWIFT_VERSION = 4.2/SWIFT_VERSION = 5.0/g' OHHTTPStubs.xcodeproj/project.pbxproj
 fi
 
-# workaround for the problem on xcode 10: https://stackoverflow.com/questions/52987843/carthage-fails-to-start-when-running-from-xcode-10-build-pre-action/53136006
-unset LLVM_TARGET_TRIPLE_SUFFIX
-
 # xcodebuild path
 xcodebuild \
  -workspace OHHTTPStubs.xcworkspace \
- -scheme "OHHTTPStubs ${buildPlatform} Framework" \
- -sdk iphonesimulator \
+ -scheme "OHHTTPStubs ${buildPlatformScheme} Framework" \
+ -sdk ${buildSdk} \
  -configuration Debug ONLY_ACTIVE_ARCH=NO \
  clean build CONFIGURATION_BUILD_DIR="../${buildPlatform}/OHHTTPStubs"
