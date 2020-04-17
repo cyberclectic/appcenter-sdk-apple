7d1e5628d65cf7b54597d96594a842c421ab95aa
Ruslan Urmeev <v-ruurme@microsoft.com>
2020-04-17 16:10:00 +0400

    add script

---
+++ Scripts/build-ohttpstubs.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+set -e
+
+echo "Build OHTTPStubs $1"
+
+# maxXcodeVersion = 1100
+swiftVersion = "5.0"
+buildPlatform = $1
+
+if [ ${XCODE_VERSION_ACTUAL} -lt 1100 ]
+then
+    #xcode 10
+    swiftVersion = "4.1"
+fi
+
+cd Vendor/OHHTTPStubs
+rake "build_carthage_frameworks[$buildPlatform,$swiftVersion]"
+cp -R "Vendor/OHHTTPStubs/Carthage/Build/${buildPlatform}/OHHTTPStubs.framework" "../../Vendor/${buildPlatform}/Hello145"
