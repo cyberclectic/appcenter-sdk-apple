f5972945d3734d9d7cf1f38620100cdec52a950d
Ruslan Urmeev <v-ruurme@microsoft.com>
2020-04-17 17:07:57 +0400

    update script

--- Scripts/build-ohttpstubs.sh
+++ Scripts/build-ohttpstubs.sh
@@ -6,17 +6,23 @@ set -e
 
 echo "Build OHTTPStubs $1"
 
-# maxXcodeVersion = 1100
+maxXcodeVersion=1100
 swiftVersion="5.0"
 buildPlatform=$1
 
-if [ ${XCODE_VERSION_ACTUAL} -lt 1100 ]
+if [ ${XCODE_VERSION_ACTUAL} -lt ${maxXcodeVersion} ]
 then
     #xcode 10
-    swiftVersion="4.1"
+    swiftVersion="4.2"
 fi
 
-cd ../Vendor/OHHTTPStubs
-# rake "build_carthage_frameworks[${buildPlatform},${swiftVersion}]"
-rake "build_carthage_frameworks[iOS,4.1]"
-# cp -R "Vendor/OHHTTPStubs/Carthage/Build/${buildPlatform}/OHHTTPStubs.framework" "../../Vendor/${buildPlatform}/Hello145"
+# workaround for the problem on xcode 10: https://stackoverflow.com/questions/52987843/carthage-fails-to-start-when-running-from-xcode-10-build-pre-action/53136006
+unset LLVM_TARGET_TRIPLE_SUFFIX
+
+# xcodebuild path
+xcodebuild \
+ -workspace ../Vendor/OHHTTPStubs/OHHTTPStubs.xcworkspace \
+ -scheme "OHHTTPStubs ${buildPlatform} Framework" \
+ -sdk iphonesimulator \
+ -configuration Debug ONLY_ACTIVE_ARCH=NO SWIFT_VERSION=${swiftVersion} \
+ clean build CONFIGURATION_BUILD_DIR="../${buildPlatform}/OHHTTPStubs"
